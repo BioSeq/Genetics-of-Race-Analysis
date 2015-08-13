f1680edd32cc8826fce5871abccd27669ba7914a
pbraunstein <pbraunstein12@gmail.com>
2015-08-13 14:11:46 -0400

    Updated so that variants are actually found in the HVRI region

--- gORAnalysis.py
+++ gORAnalysis.py
@@ -4,12 +4,40 @@ from sys import argv
 from sys import exit
 import os
 
-# CONSTANTS
+#############  CONSTANTS  #################
+# Files
 REF1 = "HVRI.fasta"
 REF2 = "HVRII.fasta"
 CONFIG = "config.txt"
-
 DATA_LOC = "../GeneticsOfRace/"
+
+# Indexes
+POS_IDX = 1
+REF_IDX = 3
+ALT_IDX = 4
+FILTER_IDX = 6
+
+# Strings
+PASS = "PASS"
+
+# Numerical Constants
+HVRI_OFFSET = 15951
+##########################################
+
+
+# CUSTOM EXCEPTIONS
+class UserException(Exception):
+    def __init__(self, msg):
+        self.msg = msg
+    def __str__(self):
+        return str(self.msg)
+
+class InternalException(Exception):
+    def __init__(self, msg):
+        self.msg = msg
+    def __str__(self):
+        return repr(self.msg)
+
 
 def main():
     ref1 = readInRef(REF1)
@@ -69,20 +97,101 @@ def makeFASTAs(path, pairs, ref1, ref2):
 
         # Figure out which region the VCF file comes from
         if nub in firsts:
-            newRef = makeChanges(ref1, vcf)
+            newRef = makeChanges(ref1, vcf, path, True)
         elif nub in seconds:
-            newRef = makeChanges(ref2, vcf)
+            newRef = makeChanges(ref2, vcf, path, False)
         else:
             continue  # vcf file not in config file, skip it
 
+        print vcf
         toReturn[nub] = newRef  
+        print newRef
 
     return toReturn
 
 
-def makeChanges(seq, vcf):
-    print "MAKEING CHANGES"
-        
+# If there are multiple alleles for the same variant, it makes the greedy
+# choice and choses the first one by default
+# When the variant file is from HVRI, need to throw out records below a
+# threshhold
+# SNPs prioritized over INDELs
+def makeChanges(seq, vcf, path, isHVRI):
+    indelStack = []
+    validSnpPoses = []
+    nucList = [x for x in seq]
+
+    with open(os.path.join(path, vcf), 'r') as filer:
+        for line in filer:
+            if line.startswith("#"):  # Skip comment lines
+                continue
+
+            listL = line.split("\t")
+            # make everything upper case so no case clashes
+            listL = [x.upper() for x in listL]
+
+            if listL[FILTER_IDX] != PASS:  # Skip vars that don't pass filter
+                continue
+
+            pos = int(listL[POS_IDX])
+            refNuc = oneInd(nucList, pos)
+            vcfOldNuc = listL[REF_IDX]
+            vcfNewNuc = listL[ALT_IDX]
+
+            # More than one allele, just grab first one
+            if "," in vcfNewNuc:
+                vcfNewNuc = vcfNewNuc.split(",")[0]
+
+
+            # These two lines protect against variants from other regions
+            # that end up in these files for some reason 
+            if isHVRI:   # Adjust for offset within file
+                pos = pos - HVRI_OFFSET
+                refNuc = oneInd(nucList, pos)  # Recalc refNuc
+                if pos < 1:
+                    continue
+                
+
+            if refNuc is None:
+                continue
+
+            ########
+
+            # Save Indels for later
+            if len(vcfOldNuc) != 1 or len(vcfNewNuc) != 1:
+                indelStack.append(listL) 
+                continue
+
+            # VCF doesn't match reference, something wrong
+            if vcfOldNuc != refNuc:
+                raise UserException("VCF REF entry doesn't match" +\
+                        " NCBI reference sequence. \nVCF: " + listL[REF_IDX] +\
+                        "\nREF: " + oneInd(nucList, pos) + "\nVCF_POS: " +\
+                        str(pos) + "\nSee VCF_File: " + vcf)
+
+            # Replace SNP
+            nucList[pos - 1] = vcfNewNuc
+            validSnpPoses.append(pos)
+
+    #    return addIndels(nucList, indelStack, validSnpPoses)
+        return "".join(nucList)
+
+
+
+# Returns the entry in the list at position pos - 1. It is used
+# to adjust between 1-indexed systems and 0-indexed systems
+def oneInd(ls, pos):
+    try:
+        return ls[pos - 1]
+    except IndexError:
+        return None
+
+
+# Inputs a list of characters that represenets a nucleotide sequence
+# a list of Indel intries and a list of positions not to add Indels in
+# as they were already included as SNPs.
+# TODO: Assumes Indels are in the reverse order of the 
+def addIndels(nucList, indelStack, exceptions):
+    return None
 
         
 if __name__ == '__main__':
